e759ec854ca116f7ff493277582ba74ea5fb557f
Janko Prester <janko.prester@gmail.com>
2019-04-17 18:23:40 +0200

    Updated api var

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,6 +1,6 @@
 // CoinAPI API config constants
 export const coinApiAddress = 'https://rest.coinapi.io';
-export const coinApiKey = process.env.REACT_APP_COINAPI_KEY || "";
+export const coinApiKey = process.env.REACT_APP_COINAPI_KEY;
 export const coinApiVersion = 'v1';
 
 // CoinMarketCap API config constants
